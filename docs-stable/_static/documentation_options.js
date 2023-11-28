2fda3d5a065e62afa992a753fc4b6bf925cea48c
niccolaimiclat <124492148+niccolaimiclat@users.noreply.github.com>
2023-11-28 18:17:54 +0800

    Re arranging the mess that is the Readthedocs index.html file
    HELP ME WHY IS EVERYTHING SO CLUTTERED 💀💀💀💀💀💀💀

---
+++ docs-stable/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '10.11.0',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'readthedocssinglehtmllocalmedia',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
